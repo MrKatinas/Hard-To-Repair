0bbb8ee82fc1aa2afcb4c3de2810dcbd1e2008aa
Mindaugas Butkus <mr.mindaugas,butkus@gmail.com>
2020-02-01 15:10:32 +0100

    Added global settings

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Settings.asset", menuName = "CreateSettings")]
+public class Settings : ScriptableObject
+{
+    #region Singleton
+
+    private const string settingsPath = "Settings";
+
+    private static Settings _settings;
+    public static Settings Get
+    {
+        get
+        {
+            if (_settings != null) return _settings;
+
+            _settings = Resources.Load<Settings>(settingsPath);
+            
+            return _settings;
+        }
+    }
+
+    #endregion
+}
