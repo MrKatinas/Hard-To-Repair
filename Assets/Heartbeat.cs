8b4950d5f562bbacc25a01b17791dc5feeb283da
Sebastian Weichelt <sewe@itu.dk>
2020-02-01 20:23:39 +0100

    animations done

--- Assets/Heartbeat.cs
+++ Assets/Heartbeat.cs
@@ -6,42 +6,71 @@ public class Heartbeat : MonoBehaviour
 {
     float alpha = 0.0f;
     bool beating = false;
+    bool bleeding = false;
     public float duration = 0.5f;
 
-    public float RotationIntensity = 20.0f;
 
-    // Start is called before the first frame update
-    void Start()
+    public float RotationIntensity = 5.0f;
+    public float BeatIntensityX = 0.1f;
+    public float BeatintensityY = 0.04f;
+
+    SpriteRenderer renderer;
+
+    private void Start()
     {
-        
+
+        renderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!beating) { return; }
+        if (!beating && !bleeding) { return; }
 
         alpha += Time.deltaTime / duration;
+        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        transform.rotation = Quaternion.identity;
+        renderer.color = Color.white;
 
-        float x = Mathf.Cos(alpha * Mathf.PI * 2.0f) * 0.10f + 0.90f;
-        float y = Mathf.Cos(alpha * Mathf.PI * 2.0f) * 0.04f + 0.96f;
-        transform.localScale = new Vector3(x, y, 1.0f);
+        if (beating)
+        {
+            float x = Mathf.Cos(alpha * Mathf.PI * 2.0f) * BeatIntensityX + (1 - BeatIntensityX);
+            float y = Mathf.Cos(alpha * Mathf.PI * 2.0f) * BeatintensityY + (1 - BeatintensityY);
+            transform.localScale = new Vector3(x, y, 1.0f);
 
-        float r = Mathf.Sin(alpha * Mathf.PI) * RotationIntensity;
-        transform.rotation = Quaternion.Euler(0.0f, 0.0f, -r);
+            float r = Mathf.Sin(alpha * Mathf.PI) * RotationIntensity;
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, -r);
+        }
         
+        
+        if (bleeding)
+        {
+            float s = Mathf.Min((Mathf.Sin(alpha * Mathf.PI) * 2), 1.0f) * 0.15f + 1.0f;
+            transform.localScale = new Vector3(s, s, 1.0f);
+
+            float red = Mathf.Sin(alpha * Mathf.PI * 3) * Mathf.Sin(alpha * Mathf.PI * 3);
+            renderer.color = new Color(1.0f, 1.0f - red, 1.0f - red);
+        }
+
 
         if (alpha >= 1.0f)
         {
             beating = false;
-            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-            transform.rotation = Quaternion.identity;
+            bleeding = false;
         }
     }
 
     public void Beat()
     {
         beating = true;
+        bleeding = false;
+        alpha = 0.0f;
+    }
+
+    public void Bleed()
+    {
+        bleeding = true;
+        beating = false;
         alpha = 0.0f;
     }
 }
